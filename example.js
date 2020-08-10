c5b7cab3f784c20ec122f5cb51c901bc6f7212bc
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-08-11 07:46:05 +0800

    function as data & args

--- example.js
+++ example.js
@@ -1,23 +1,61 @@
-function myFunction(arg1, arg2) {
-  // function body
-}
+// function myFunction(arg1, arg2) {
+//   // function body
+// }
 
-const myOtherFunction = function (arg1, arg2) {
-  // function body
-}
+// const myOtherFunction = function (arg1, arg2) {
+//   // function body
+// }
+
+// const add = (x, y) => x + y;
+
+// const double = (number) => number * 2;
+
+// const sayHello = () => console.log('Hello');
+
+// const getPersonData = () => ({
+//   name: 'John Doe',
+//   age: 34,
+//   job: 'programmer'
+// });
+
+
+// Function as data
+
+const double = x => x * 2;
+const subtractOne = x => x - 1;
+const triple = x => x * 3;
+const add5 = x => x + 5;
+
+const myNumber = 42;
+
+const doubled = double(myNumber);
+const minusOne = subtractOne(doubled);
+// ...
+
+const functionsArray = [
+  double,
+  subtractOne,
+  triple,
+  add5,
+  Math.sqrt
+];
+
+var number = 42;
+
+functionsArray.forEach(func => number = func(number));
+console.log(number);
+
+// Passing functions as arguments
 
 const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
 
-const double = (number) => number * 2;
+const combine2and3 = func =>
+  func(2, 3);
 
-const double = function (number) {
-  return number * 2;
-}
+combine2and3(add); // 2 + 3
+combine2and3((x, y) => x + y)
+combine2and3(subtract); // 2 - 3
 
-const sayHello = () => console.log('Hello');
+combine2and3(Math.max) // -> 3
 
-const getPersonData = () => ({
-  name: 'John Doe',
-  age: 34,
-  job: 'programmer'
-});
