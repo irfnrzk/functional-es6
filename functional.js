7c086d9503e9d54b433df1a6092175269a9e630b
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-08-14 13:46:05 +0800

    recreating map function

--- functional.js
+++ functional.js
@@ -45,3 +45,27 @@ const createLengthTest = minLength =>
 
 const longWords = words.filter(createLengthTest(5));
 console.log(longWords);
+
+// challenge recreating map function
+
+const map = (arr, func) => {
+
+  // my answer
+  const newArr = [];
+  arr.forEach(el => {
+    return newArr.push(func(el))
+  });
+  return newArr;
+
+  // schema answer
+  // return arr.reduce((acc, x) => [
+  //   ...acc,
+  //   func(x)
+  // ], []);
+
+}
+
+// testing if it works
+console.log(map([1, 2, 3], x => x * 2)); // should be [2,4,6]
+console.log(map([-5, -6, -7, -8, -9, -10], x => -x)); // should be [5, 6, 7, 8, 9, 10]
+console.log(map(['a', 'b', 'c', 'd'], x => x.toUpperCase())); // should be ['A', 'B', 'C', 'D']
