dc49c64d72b7b2906b78fbcb08d534aa1db34d5e
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-08-14 11:41:22 +0800

    implement private variables

--- example.js
+++ example.js
@@ -87,6 +87,24 @@ const createPrinter = () => {
 const printer = createPrinter();
 printer()
 
+// implement private var
 
+const Person = ({ name, age, job }) => {
+  var _name = name;
+  var _age = age;
+  var _job = job;
 
+  return {
+    getName: () => _name,
+    getAge: () => _age,
+    getJob: () => _job,
+
+    setJob: newJob => _job = newJob
+  };
+}
+
+const me = Person({ name: 'irfan', age: '31', job: 'developer' });
+console.log(me.getJob());
+me.setJob('senior developer');
+console.log(me.getJob());
 
