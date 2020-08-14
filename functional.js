e39fea8584a315bf9e0069ab7a85283a1c6f1ede
Muhammad Irfan Ab Razak <irfan.abrazak@gmail.com>
2020-08-14 12:07:20 +0800

    spread operator

---
+++ functional.js
@@ -0,0 +1,29 @@
+// spread operator
+
+const person = {
+  name: 'Jimmy',
+  age: 40,
+  hairColor: 'brown',
+  eyeColor: 'blue'
+};
+
+const careerData = {
+  title: 'developer',
+  company: 'ABC software'
+};
+
+const personWithCareerData = {
+  ...person,
+  ...careerData
+};
+
+console.log(personWithCareerData);
+
+const numbers = [1, 2, 3, 4, 5];
+const newNumbers = [
+  0,
+  ...numbers,
+  6
+];
+
+console.log(newNumbers);
